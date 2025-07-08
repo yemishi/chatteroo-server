f1bc50a7cb8d471cf13067186aa43927335f9b16
yemishi <daviddperera10@gmail.com>
2025-07-08 20:06:09 +0100

    centralize Express app in socket server for shared usage

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import "dotenv/config";
-
+import cors from "cors";
 import express from "express";
 import mainRouter from "./index";
-const app = express();
+import { app } from "./lib/server";
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/api", mainRouter);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
