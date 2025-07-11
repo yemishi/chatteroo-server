51ca5fad36f57c409e514d078eaed720ebe09cd5
yemishi <daviddperera10@gmail.com>
2025-07-11 19:39:48 +0100

    update chat route following the new chat schema

--- src/routes/chat/chat.ts
+++ src/routes/chat/chat.ts
@@ -1,25 +1,43 @@
 import express from "express";
 import { db } from "../../lib/db";
+import { authenticate, AuthRequest } from "../../lib/auth";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.use(authenticate);
+router.get("/", async (req: AuthRequest, res) => {
   const chatId = req.query.chatId as string;
 
-  if (!chatId) {
-    res.status(400).json({ message: "Chat ID is required" });
-    return;
-  }
+  const user = req.user;
 
   try {
-    const chat = await db.chat.findUnique({ where: { id: chatId } });
+    if (chatId) {
+      const chat = await db.chat.findUnique({
+        where: { id: chatId },
+        include: { users: { select: { picture: true, username: true, bio: true } } },
+      });
 
-    if (!chat) {
-      res.status(404).json({ message: "Chat not found" });
+      res.status(200).json({ chat });
+      return;
+    }
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
       return;
     }
 
-    res.status(200).json({ chat });
+    const chats = await db.chat.findMany({
+      where: {
+        members: {
+          has: user.id,
+        },
+      },
+      include: {
+        messages: { orderBy: { timestamp: "desc" }, take: 1 },
+        users: { select: { picture: true, username: true, bio: true } },
+      },
+    });
+
+    res.status(200).json({ chats });
     return;
   } catch (error) {
     console.error("GET /chat error:", error);
