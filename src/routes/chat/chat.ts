d4f114c6e249728be6df6e969ff77f29cd30afd9
yemishi <daviddperera10@gmail.com>
2025-07-15 20:33:57 +0100

    improve routes by adding pagination

--- src/routes/chat/chat.ts
+++ src/routes/chat/chat.ts
@@ -6,20 +6,13 @@ const router = express.Router();
 
 router.use(authenticate);
 router.get("/", async (req: AuthRequest, res) => {
-  const chatId = req.query.chatId as string;
-
+  const { take = 20, page = 0 } = req.query as {
+    take?: number;
+    page?: number;
+  };
   const user = req.user;
 
   try {
-    if (chatId) {
-      const chat = await db.chat.findUnique({
-        where: { id: chatId },
-        include: { users: { select: { picture: true, username: true, bio: true } } },
-      });
-
-      res.status(200).json({ chat });
-      return;
-    }
     if (!user) {
       res.status(404).json({ message: "User not found" });
       return;
@@ -35,15 +28,36 @@ router.get("/", async (req: AuthRequest, res) => {
         messages: { orderBy: { timestamp: "desc" }, take: 1 },
         users: { select: { picture: true, username: true, bio: true } },
       },
+      skip: Number(page) * Number(take),
+      take,
     });
 
     res.status(200).json({ chats });
     return;
   } catch (error) {
     console.error("GET /chat error:", error);
-    res.status(500).json({ message: "Failed to fetch chat." });
+    res.status(500).json({ message: "Failed to fetch chats." });
     return;
   }
 });
 
+router.get("/:chatId", async (req, res) => {
+  const { chatId } = req.params;
+  try {
+    if (!chatId) {
+      res.status(404).json({ message: "Chat not found" });
+      return;
+    }
+    const chat = await db.chat.findUnique({
+      where: { id: chatId as string },
+      include: { users: { select: { picture: true, username: true, bio: true } } },
+    });
+
+    res.status(200).json({ chat });
+    return;
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch chat." });
+    return;
+  }
+});
 export default router;
