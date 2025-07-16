0ad85fe09399bce314bbfae2700de72e14b93163
yemishi <daviddperera10@gmail.com>
2025-07-16 20:14:16 +0100

    removes global route and create two for send-friend-request and received-friend-request

--- src/routes/friend/request/request.ts
+++ src/routes/friend/request/request.ts
@@ -7,44 +7,42 @@ const router = express.Router();
 
 router.use(authenticate);
 
-router.get("/", async (req: AuthRequest, res) => {
-  const { userId } = req.params;
-  const currentUser = req.user;
+router.get("/sends/:userId", async (req: AuthRequest, res) => {
+  const id = req.query.userId ? req.query.userId : req.user?.id;
+  const take = Number(req.query.take ?? 20);
+  const page = Number(req.query.page ?? 0);
 
-  const { take = 20, page = 0 } = req.query as { take?: number; page?: number };
+  await handleRequestList({
+    userId: id as string,
+    relationField: "fromId",
+    includeField: "to",
+    res,
+    take,
+    page,
+  });
+});
 
-  if (!currentUser) {
-    res.status(401).json({ message: "Unauthorized" });
-    return;
-  }
+router.get("/receives", async (req: AuthRequest, res) => {
+  const id = req.query.userId ? req.query.userId : req.user?.id;
+  const take = Number(req.query.take ?? 20);
+  const page = Number(req.query.page ?? 0);
 
-  try {
-    const skip = Number(page) * Number(take);
-    const requests = await db.request.findMany({
-      where: { to: userId, status: "pending" },
-      skip,
-      take,
-    });
-
-    res.status(200).json({
-      requests,
-    });
-  } catch (err) {
-    console.error("GET /requests error:", err);
-    res.status(500).json({ message: "We had an error trying to retrieve the friend requests" });
-  }
+  await handleRequestList({
+    userId: id as string,
+    relationField: "toId",
+    includeField: "from",
+    res,
+    take,
+    page,
+  });
 });
 
 router.post("/:userId", async (req: AuthRequest, res) => {
   const { userId } = req.params;
   const currentUser = req.user;
 
-  if (!currentUser) {
-    res.status(401).json({ message: "Unauthorized" });
-    return;
-  }
   try {
-    await db.request.create({ data: { from: currentUser.id, to: userId } });
+    await db.request.create({ data: { fromId: currentUser?.id!, toId: userId } });
     res.status(201).json({ message: "Friend request sent successfully." });
     return;
   } catch (err) {
@@ -68,7 +66,7 @@ router.delete("/:requestId", async (req: AuthRequest, res) => {
       where: { id: requestId },
     });
 
-    if (!request || request.to !== currentUser.id) {
+    if (!request || request.toId !== currentUser.id) {
       res.status(404).json({ message: "Request not found or unauthorized." });
       return;
     }
@@ -95,12 +93,19 @@ router.patch("/:requestId/accept", async (req: AuthRequest, res) => {
       where: { id: requestId },
     });
 
-    if (!request || request.to !== currentUser.id) {
+    if (!request || request.toId !== currentUser.id) {
       res.status(404).json({ message: "Friend request not found or unauthorized." });
       return;
     }
 
-    const friendId = request.from;
+    const friendId = request.fromId;
+    const existingChat = await db.chat.findFirst({
+      where: {
+        members: {
+          hasEvery: [currentUser.id, friendId],
+        },
+      },
+    });
 
     await db.$transaction([
       db.user.update({
@@ -111,13 +116,16 @@ router.patch("/:requestId/accept", async (req: AuthRequest, res) => {
         where: { id: friendId },
         data: { friends: { push: currentUser.id } },
       }),
-      db.request.update({
+      db.request.delete({
         where: { id: requestId },
-        data: { status: "accepted" },
       }),
-      db.chat.create({
-        data: { members: { set: [currentUser.id, friendId] } },
-      }),
+      ...(existingChat
+        ? []
+        : [
+            db.chat.create({
+              data: { members: { set: [currentUser.id, friendId] } },
+            }),
+          ]),
     ]);
 
     res.status(200).json({ message: "Friend request accepted successfully." });
@@ -126,5 +134,47 @@ router.patch("/:requestId/accept", async (req: AuthRequest, res) => {
     res.status(500).json({ message: "Failed to accept friend request." });
   }
 });
+
+async function handleRequestList({
+  userId,
+  relationField,
+  includeField,
+  res,
+  take,
+  page,
+}: {
+  userId: string;
+  relationField: "fromId" | "toId";
+  includeField: "from" | "to";
+  res: any;
+  take: number;
+  page: number;
+}) {
+  const skip = page * take;
+  try {
+    const [count, requests] = await db.$transaction([
+      db.request.count({ where: { [relationField]: userId } }),
+      db.request.findMany({
+        where: { [relationField]: userId },
+        skip,
+        take,
+        include: {
+          [includeField]: {
+            select: { username: true, picture: true, guestId: true },
+          },
+        },
+      }),
+    ]);
+    res.status(200).json({
+      requests,
+      hasMore: skip + requests.length < count,
+    });
+  } catch (err) {
+    console.error(`GET /requests/${relationField} error:`, err);
+    res.status(500).json({
+      message: "We had an error trying to retrieve the friend requests.",
+    });
+  }
+}
 
 export default router;
