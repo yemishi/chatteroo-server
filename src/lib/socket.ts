ac5c25c1b90bba8762ddaf10a82575174afa50de
yemishi <daviddperera10@gmail.com>
2025-07-22 15:50:47 +0100

    add new route for subscribe to all user rooms

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -22,9 +22,16 @@ io.on("connection", (socket: Socket) => {
   console.log("✅ User connected:", socket.id);
 
   const userId = socket.handshake.query.userId as string | undefined;
+
   if (userId) {
     onlineUsers[userId] = socket.id;
   }
+  socket.on("subscribe-all", (chatIds: string[]) => {
+    for (const id of chatIds) {
+      socket.join(id);
+      console.log("joined", id);
+    }
+  });
 
   io.emit("online-users", Object.keys(onlineUsers));
   socket.on("getOnlineUsers", () => {
@@ -41,9 +48,22 @@ io.on("connection", (socket: Socket) => {
     socket.leave(roomId);
   });
 
-  socket.on("send", async ({ message }) => {
-    console.log(`${message.sender} sent message to ${message.room}: ${message.content}`);
+  socket.on("send-message", async ({ message, membersId }) => {
     io.to(message.room).emit("message", message);
+
+    for (const userId of membersId) {
+      const socketId = onlineUsers[userId];
+      if (socketId && socketId !== socket.id) {
+        io.to(socketId).emit("chat-updated", {
+          chatId: message.room,
+          lastMessage: {
+            content: message.content,
+            senderId: message.senderId,
+            timestamp: new Date().toISOString(),
+          },
+        });
+      }
+    }
 
     try {
       await db.message.create({
