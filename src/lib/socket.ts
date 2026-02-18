db622c3b5bcccb8bd4904853db55bd1dbb836918
yemishi <daviddperera10@gmail.com>
2026-02-18 19:46:47 +0000

    enhance messaging and typing events with notifications

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -20,17 +20,17 @@ export function getUserSocketId(userId: string): string | undefined {
 
 io.on("connection", (socket: Socket) => {
   const userId = socket.handshake.query.userId as string | undefined;
-  console.log("✅ User connected:", userId, "AAAA");
+  console.log("✅ User connected:", userId);
 
   if (userId) {
     onlineUsers[userId] = socket.id;
   }
-  socket.on("subscribe-all", (chatIds: string[]) => {
+  /*   socket.on("subscribe-all", (chatIds: string[]) => {
     for (const id of chatIds) {
       socket.join(id);
       console.log("joined", id);
     }
-  });
+  }); */
 
   io.emit("online-users", Object.keys(onlineUsers));
   socket.on("getOnlineUsers", () => {
@@ -47,29 +47,71 @@ io.on("connection", (socket: Socket) => {
     socket.leave(roomId);
   });
 
-  socket.on("send-message", async ({ message, membersId }) => {
+  type Message = {
+    room: string;
+    senderId: string;
+    senderName: string;
+    senderPicture: string;
+    content: { text?: string; imgs: string[] };
+    timestamp: Date;
+  };
+
+  socket.on("typing", ({ roomId, userId, usersId }: { roomId: string; userId: string; usersId: string[] }) => {
+    for (const id of usersId) {
+      const socketId = onlineUsers[id];
+      if (socketId && socketId !== socket.id) {
+        io.to(socketId).emit("user-typing", { roomId, userId });
+      }
+    }
+  });
+
+  socket.on("stop-typing", ({ roomId, userId, usersId }: { roomId: string; userId: string; usersId: string[] }) => {
+    for (const id of usersId) {
+      const socketId = onlineUsers[id];
+      if (socketId && socketId !== socket.id) {
+        io.to(socketId).emit("user-stop-typing", { roomId, userId });
+      }
+    }
+  });
+
+  socket.on("edit-message", async ({ message, chatRoom }) => {
+    io.to(chatRoom).emit("update-message", message);
+  });
+  socket.on("send-message", async ({ message, membersId }: { message: Message; membersId: any[] }) => {
     const timestamp = new Date();
-    io.to(message.room).emit("message", { ...message, timestamp });
+    const newMessage = await db.message.create({
+      data: {
+        content: message.content,
+        senderId: message.senderId,
+        chatId: message.room,
+      },
+    });
+
+    io.to(message.room).emit("message", { ...message, timestamp, id: newMessage.id });
     for (const userId of [...membersId, socket.handshake.query.userId]) {
       const socketId = onlineUsers[userId];
+
       if (socketId) {
         io.to(socketId).emit("chat-updated", {
           chatId: message.room,
           content: message.content,
           senderId: message.senderId,
+
           timestamp,
         });
+
+        if (!io.sockets.adapter.rooms.get(message.room)?.has(socketId)) {
+          io.to(socketId).emit("toast", {
+            title: message.senderName,
+            icon: message.senderPicture,
+            chatId: message.room,
+            message: message.content,
+          });
+        }
       }
     }
 
     try {
-      await db.message.create({
-        data: {
-          content: message.content,
-          senderId: message.senderId,
-          chatId: message.room,
-        },
-      });
       await db.chat.update({
         where: { id: message.room },
         data: { lastMessageAt: timestamp },
